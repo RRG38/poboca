75c06396889a30a967387e2ab522358f8b55a886
Ryan Garren <rryangarren@gmail.com>
2022-01-22 14:57:26 -0700

    spelling corrections

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -84,7 +84,7 @@ class Auth extends Component {
 
           <input className="auth-input"
             value={this.state.school}
-            placeholder="Bootcamp (Only at Register)"
+          placeholder="Bootcamp Attended"
             onChange={(e) => this.handleChange("school", e.target.value)}
           />
             <input className="auth-input"
